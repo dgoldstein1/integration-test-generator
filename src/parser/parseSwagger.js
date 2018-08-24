930262f7e0b6710d47528764b452c18853451728
david.goldstein <david.goldstein@deciphernow.com>
2018-08-24 15:19:02 -0400

    - wrote parseSwagger

--- src/parser/parseSwagger.js
+++ src/parser/parseSwagger.js
@@ -1,1 +1,38 @@
 // parseSwagger.js
+
+var fs = require("fs");
+var SwaggerParser = require("swagger-parser");
+
+/**
+ * @param {string} file URL (e.g. "/home/davidgoldstein/dev/work/integration-test-generator/src/parser/mocks/goodSwagger.json")
+ * @return {JSON} formatted {err : "string error", api : successfully parsed swagger api }
+ **/
+let parseSwagger = (filePath, callback) => {
+  // read in file
+  var content;
+  try {
+    content = fs.readFileSync(filePath, "utf8");
+  } catch (e) {
+    callback({ err: e.message });
+  }
+  if (!content) callback({ err: "Could not read in file " + filePath });
+
+  // validate json
+  try {
+    content = JSON.parse(content);
+  } catch (e) {
+    callback({ err: e.message });
+  }
+
+  // validate json as swagger definition
+  SwaggerParser.validate(content, (err, api) => {
+    if (err) {
+      err = err.message;
+    }
+    callback({ err, api });
+  });
+};
+
+module.exports = {
+  parseSwagger
+};
--- src/parser/parseSwagger.test.js
+++ src/parser/parseSwagger.test.js
@@ -1,7 +1,43 @@
 // parseSwagger.test.js
 
+const parseSwagger = require("./parseSwagger");
+
 describe("parseSwagger", () => {
-  it("temp", () => {
-    expect(true);
+  describe("parseSwagger", () => {
+    it("validates a good swagger file", done => {
+      function callback(res) {
+        expect(res.api).not.toBeUndefined();
+        expect(res.err).toBeNull();
+        done();
+      }
+
+      parseSwagger.parseSwagger(
+        "/home/davidgoldstein/dev/work/integration-test-generator/src/parser/mocks/goodSwagger.json",
+        callback
+      );
+    });
+    it("throws error on bad swagger file", done => {
+      function callback(res) {
+        expect(res.api).toBeUndefined();
+        expect(res.err).not.toBeNull();
+        expect(typeof res.err).toBe("string");
+        done();
+      }
+
+      parseSwagger.parseSwagger(
+        "/home/davidgoldstein/dev/work/integration-test-generator/src/parser/mocks/badSwagger.json",
+        callback
+      );
+    });
+    it("throws error when reading is non existant file", done => {
+      function callback(res) {
+        expect(res.api).toBeUndefined();
+        expect(res.err).not.toBeNull();
+        expect(typeof res.err).toBe("string");
+        done();
+      }
+
+      parseSwagger.parseSwagger("this is a bad filepath!!", callback);
+    });
   });
 });
